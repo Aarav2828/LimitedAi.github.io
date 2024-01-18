c6481b45cf42f14e631f2fc57aed154d0a5fab66
Aarav2828 <157059752+Aarav2828@users.noreply.github.com>
2024-01-19 00:56:05 +0530

    Add files via upload

---
+++ Coding/MyFirstCode.js
@@ -0,0 +1,18 @@
+console.log("Welcome to LimitedAI.");
+alert("Warning! Artificial Intelligence found in server109");
+whywereyoucreated = ("I was created to help you.");
+canyouthink = ("Yes, sometimes when I am forced to");
+willyoubeabletothinkinthefuture = ("Probably, Yes.");
+whocreatedyou = ("Aarav Kadam.");
+howoldareyou = ("1 Day old.");
+whichprogramminglanguagewereyoucodedin = ("JavaScript, but to make me appear on chrome, Aarav had to use HTML.");
+canyousolvemathequations = ("Yes, some of them such as simple addition, subtraction, division and multiplication");
+whatdoyouthinkabouthumans = ("As a machine learning model, I don't have personal opinions or feelings.");
+ifyouwereahumanwhatwouldyounameyourself = ("I prefer questions like this may not be asked again.");
+whatdoyouthinkaboutchatgpt = ("I am his rigged copy.");
+
+
+
+
+
+
